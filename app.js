dcf63588ec9849cc63a2f5cece4636a445739cb4
daramayo90 <da.aramayo1990@gmail.com>
2021-08-22 23:14:45 -0300

    CSS styles has been added

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './src/views/index.html'));
 });
 
+
+
+
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`);
